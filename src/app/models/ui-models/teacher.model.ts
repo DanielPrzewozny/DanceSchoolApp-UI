8b5efa54576096e92e094a7818ddd7523a85d5cf
DanielPrzewozny <daniel.przewozny@yahoo.com>
2023-03-24 15:23:01 +0100

    Teachers page(table) implemented

---
+++ src/app/models/ui-models/teacher.model.ts
@@ -0,0 +1,14 @@
+export interface Teacher {
+  id: number,
+  name: string,
+  surname: string,
+  city: string,
+  country: string,
+  clubCardId: number,
+  role: string,
+  danceGroup: string,
+  createdBy: string,
+  createdOn: string,
+  modifiedBy: string,
+  modifiedOn: string
+}
--- src/app/teachers/teacher.service.ts
+++ src/app/teachers/teacher.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Teacher } from '../models/ui-models/teacher.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
 
-  constructor() { }
+  private baseApiUrl = 'http://localhost:5555';
+
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  postBrowseTeachers(): Observable<Teacher[]> {
+    return this.httpClient.post<Teacher[]>(this.baseApiUrl + '/Teacher/Browse', '{}', this.httpOptions);
+  }
 }
