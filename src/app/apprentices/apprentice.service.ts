3fa106e617d82c7320a2e9d69ce52cf3d48aa469
DanielPrzewozny <daniel.przewozny@yahoo.com>
2023-03-24 15:58:26 +0100

    Code cleanup, bugfixes

--- src/app/apprentices/apprentice.service.ts
+++ src/app/apprentices/apprentice.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Apprentice } from '../models/api-models/apprentice.model';
-import { Teacher } from '../models/api-models/teacher.model';
 
 @Injectable({
   providedIn: 'root'
