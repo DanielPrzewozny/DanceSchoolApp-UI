c8e3fe57649343e59a02d49cb8b914ba3989a5d2
DanielPrzewozny <daniel.przewozny@yahoo.com>
2023-03-24 21:09:25 +0100

    Delete action implemented (for lessons obj)

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -4,6 +4,8 @@ import { LessonService } from './lesson.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lessons',
@@ -12,13 +14,16 @@ import { MatSort } from '@angular/material/sort';
 })
 export class LessonsComponent implements OnInit {
   lessons: Lesson[] = [];
-  displayedColumns: string[] = ['id', 'danceGroup', 'name', 'teacherId', 'timeEst','description', 'everySpecificDayOfWeek', 'createdBy','createdOn', 'modifiedBy', 'modifiedOn','edit'];
+  displayedColumns: string[] = ['id', 'danceGroup', 'name', 'teacherId', 'timeEst','description', 'everySpecificDayOfWeek',
+  'createdBy','createdOn', 'modifiedBy', 'modifiedOn','edit', 'delete'];
   dataSource = new MatTableDataSource<Lesson>(this.lessons);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) matSort!: MatSort;
 
-  constructor(private lessonsService: LessonService){}
+  constructor(private lessonsService: LessonService,
+    private snackbar: MatSnackBar,
+    private router: Router){}
 
   ngOnInit(): void {
     //Fetch Lessons
@@ -40,4 +45,18 @@ export class LessonsComponent implements OnInit {
         }
       )
   }
+
+  onDelete(lessonId: string):void {
+    this.lessonsService.deleteLesson(lessonId)
+      .subscribe(
+        (successResponse) => {
+          console.log(successResponse);
+          setTimeout(() => {window.location.reload();}, 2000);
+          this.snackbar.open('Lesson successfully deleted', undefined, {duration: 2000});
+        },
+        (errorResponse) => {
+          console.log(errorResponse);
+        }
+      )
+    }
 }
